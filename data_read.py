a8f791f1cd1f3694d2c38e6dce7301ea71831f54
GordonRV <GordonRV@210-roman.upkb.lokal>
2013-11-18 16:47:40 +0600

    Закончен и проверен модуль рассчета свертки из сигнала. Сверено с матлабом.

--- data_read.py
+++ data_read.py
@@ -21,10 +21,13 @@ import struct
 import os
 import matplotlib.pyplot as plt
 import time
+import swertka
+import numpy as np
 
 data_fname = "data/02.bin"
 # (66, 83, 73, 64, 100, 50, 49, 48)
 
+# quantity of blocks
 block_num = os.path.getsize(data_fname) // data_config.packet_length
 
 def get_vars(df, block_num):
@@ -41,12 +44,12 @@ def get_vars(df, block_num):
             else:               # array
                 s1 = "<%d%s" % (item[2], _data_type[item[1]])
                 p = packet[index:(index+item[1]*item[2])]
-                v = struct.unpack(s1, p)
+                v = np.asarray(struct.unpack(s1, p))
                 variables.update({s: v})
-
             index = index + item[1] * item[2]
         yield variables
 
+print('Block quantity = %d' % block_num)
 print('Start...')
 t1 = time.perf_counter()
 
@@ -55,23 +58,35 @@ data_file = open(data_fname, "rb")
 lHi = []
 
 #plt.ion()
-for i in get_vars(data_file, block_num):
-    lHi.append(i['Hi'] / 8)
+for i in get_vars(data_file, 1000):
+#for i in get_vars(data_file, block_num):
+    #print(i['Swertka'])
+    res = swertka.get_swertka(i['CodNonius'], i['Num_Swr'], i['Num_Div'],
+            i['Diapazon'], i['Srez'])
+
+#    lHi.append(i['Hi'] / 8)
 #    plt.clf()
+#    plt.plot(res)
 #    plt.plot(i['Swertka'][0:i['Num_Swr']])
 #    plt.draw()
 #plt.close()
 
 #    print(i["Hi"])
-#    if (i['Npack_'] % 1000) == 0:
-#        print(i["Npack_"])
+    #if (i['Npack_'] % 1000) == 0:
+    if (i['Npack_'] % 100) == 0:
+        print(i["Npack_"])
+
+#plt.plot(res)
+#plt.plot(i['Swertka'][0:i['Num_Swr']])
+#plt.show()
+
 data_file.close()
 
 t2 = time.perf_counter() - t1
 print('Stop! Elapsed time is %f s' % t2)
 
-plt.plot(lHi)
-plt.show()
+#plt.plot(lHi)
+#plt.show()
 
 
 
--- swertka.py
+++ swertka.py
@@ -15,40 +15,112 @@
 #if __name__ == '__main__':
 #    main()
 
+import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 def get_swertka(CodNonius, Num_Swr, Num_Div, Diapazon, Srez):
     _noise = 8
     _gr_non = 2 ** (5 - CodNonius)
     _bad = 0
     maxx = Num_Swr * Num_Div * 2
-    if maxx > len(Srez):
-        sk = np.ones(Num_Swr, dtype=np.int) # tuple([1 for i in range(Num_Swr)])
-        s1 = np.ones(Num_Swr, dtype=np.int) # tuple([1 for i in range(Num_Swr)])
-        c1 = np.ones(Num_Swr, dtype=np.int) # tuple([1 for i in range(Num_Swr)])
-        zz = np.ones(Num_Swr, dtype=np.int) # tuple([1 for i in range(Num_Swr)])
+#    if maxx > len(Srez):
+    if maxx > Srez.size:
+        sk = np.ones(Num_Swr, dtype=np.int)
+        s1 = np.ones(Num_Swr, dtype=np.int)
+        c1 = np.ones(Num_Swr, dtype=np.int)
+        zz = np.ones(Num_Swr, dtype=np.int)
         Signal = 1
         bad = 1
         # Exception? Returned value?
         return False
 
-    _srez = np.array(Srez[0:maxx])
-    _srez = _srez.reshape(maxx//4, 4)
-    _srez = np.fliplr(_srez)
-    _srez = _srez.reshape(1, maxx)
+#    _srez = Srez[0:maxx]
+#    _srez = _srez.reshape(4, maxx//4, order='F')
+#    _srez = np.flipud(_srez)
+#    _srez = _srez.reshape(1, maxx, order='F')
 
-    Sz = _srez.reshape(Num_Swr//(_gr_non // 2), _gr_non)
+    _srez = Srez[0:maxx]
+    _srez = np.flipud(_srez.reshape(4, maxx//4, order='F'))
+    _srez = _srez.reshape(1, maxx, order='F')
+
+    Sz = _srez.reshape(Num_Swr//(_gr_non // 2), _gr_non, maxx // (Num_Swr * 2), order='F')
+
     Szs = np.zeros(Num_Swr * Num_Div)
     Szc = np.zeros(Num_Swr * Num_Div)
 
+    for k in range(Num_Div):
+        Sz1 = Sz[:,:,k]
+        Sz_buf = np.zeros(Num_Swr)
+        i = 0
+        j = 0
+        while i < Num_Swr:
+            for n in range(_gr_non//2):
+                Sz_buf[i+n] = Sz1[j, (_gr_non-2*n)-1]
+            i = i + _gr_non//2
+            j = j + 1;
+        Szc[k * Num_Swr : k*Num_Swr + Num_Swr] = Sz_buf
+
+    for k in range(Num_Div):
+        Sz1 = Sz[:,:,k]
+        Sz_buf = np.zeros(Num_Swr)
+        i = 0
+        j = 0
+        while i < Num_Swr:
+            for n in range(_gr_non//2):
+                Sz_buf[i+n] = Sz1[j, (_gr_non-2*n)-2]
+            i = i + _gr_non//2
+            j = j + 1;
+        Szs[k * Num_Swr : k*Num_Swr + Num_Swr] = Sz_buf
+
+    #Signal = [math.sqrt( (c - np.mean(Szc))**2 +
+    #            (s - np.mean(Szs))**2) for c, s in zip(Szc, Szs)]
+    Signal = np.sqrt((np.square(Szc - np.mean(Szc))) + (np.square(Szs - np.mean(Szs))))
+
+    s1 = Szs.reshape(Num_Swr, Num_Div, order='F')
+    c1 = Szc.reshape(Num_Swr, Num_Div, order='F')
+
+    #msk = [n / Num_Div for n in [sum(item) for item in s1]]
+    #msk = np.asarray([n / Num_Div for n in np.sum(s1, axis=1)])
+    msk =  np.floor(np.sum(s1, axis=1) / Num_Div)
+    s2 = s1[0:_noise,:]
+    #ms0 = np.mean([np.mean(item) for item in s2])
+    ms0 = np.floor(np.mean(s2))
+
+    #mck = [n / Num_Div for n in [sum(item) for item in c1]]
+    #mck = np.asarray([n / Num_Div for n in np.sum(c1, axis=1)])
+    mck = np.floor(np.sum(c1, axis=1) / Num_Div)
+    c2 = c1[0:_noise,:]
+    #mc0 = np.mean([np.mean(item) for item in c2])
+    mc0 = np.floor(np.mean(c2))
+    n20 = (np.square(ms0) + np.square(mc0))
+
+    #s2 = [n**2 for n in s2]
+    #c2 = [n**2 for n in c2]
+    s2 = np.square(s2)
+    c2 = np.square(c2)
+    #sc2 = [m + n for m, n in zip(s2, c2)]
+    sc2 = s2 + c2
+
+    #m20 = [np.mean(n) for n in [np.mean(m) for m in sc2]]
+    #m20 = [np.mean(n) for n in np.mean(sc2, axis=1)]
+    #m20 = np.mean(np.mean(sc2, axis=1), axis=0)
+    m20 = np.floor(np.mean(sc2))
+
+    #s2 = [n**2 for n in s1]
+    #c2 = [n**2 for n in c1]
+    s2 = np.square(s1)
+    c2 = np.square(c1)
+    #sc2 = [m + n for m, n in zip(s2, c2)]
+    sc2 = s2 + c2
+
+    #m2k = [np.mean(m) for m in sc2]
+    m2k = np.floor(np.mean(sc2, axis=1))
+
+    #sk =[m2_ - 2*(s*ms0 + c*mc0) + n20 for s, c, m2_ in zip(msk, mck, m2k)]
+    sk =  (m2k - 2*(msk*ms0 + mck*mc0)) + n20
+
+    return sk
 
 
-
-
-
-
-
-#test_srez = [i for i in range(128)]
-#res =  get_swertka(1, 16, 4, 1, test_srez)
-#print(res)
 
