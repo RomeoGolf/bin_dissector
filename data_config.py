378229719219b1b428a4baa0f0b99f152e8304cd
RomeoGolf <RomeoGolf@local>
2014-05-17 22:51:50 +0600

    Модуль приведен к объектному виду

--- data_config.py
+++ data_config.py
@@ -1,36 +1,44 @@
 
-config_fname = "data/config_2.txt"     # name of configuration file
-
-# get config strings
-config_file = open(config_fname, "rt")
-lines = config_file.readlines()
-config_file.close()
-
-#get config list
-config = [];    # dummy for config list
-for line in lines:
-    var_config = line.strip().split()
-    if len(var_config) == 3:
-        var_name = var_config[0]
-        var_width = int(var_config[1]) - 1
-        var_length = int(var_config[2])
-        var_config = var_name, var_width, var_length
-        config.append(var_config)
-    if len(var_config) == 2:
-        var_name = var_config[0]
-        if var_name != "unused":
-            raise RuntimeError('Wrong name for unused bytes!')
-        var_width = 1
-        var_length = int(var_config[1])
-        var_config = var_name, var_width, var_length
-        config.append(var_config)
-
-#get data packet length
-packet_length = 0
-for item in config:
-    packet_length = packet_length + item[1] * item[2]
-
-#print("%dd %xh" % (packet_length, packet_length))
 
 
+class DataConfig:
+    def __init__(self, fname):
+        self.config_fname = fname
+
+    def Barrr(self):
+        print('bar')
+
+    def GetConfigStrings(self):
+        self.config_file = open(self.config_fname, "rt")
+        self.lines = self.config_file.readlines()
+        self.config_file.close()
+
+    def GetConfigList(self):
+        self.config = [];    # dummy for config list
+        for line in self.lines:
+            var_config = line.strip().split()
+            if len(var_config) == 3:
+                var_name = var_config[0]
+                var_width = int(var_config[1]) - 1
+                var_length = int(var_config[2])
+                var_config = var_name, var_width, var_length
+                self.config.append(var_config)
+            if len(var_config) == 2:
+                var_name = var_config[0]
+                if var_name != "unused":
+                    raise RuntimeError('Wrong name for unused bytes!')
+                var_width = 1
+                var_length = int(var_config[1])
+                var_config = var_name, var_width, var_length
+                self.config.append(var_config)
+        return self.config
+
+    def GetDataPacketLength(self):
+        self.packet_length = 0
+        for item in self.config:
+            self.packet_length = self.packet_length + item[1] * item[2]
+        return self.packet_length
+
+    def Foo(self):
+        print('qwerty')
 
