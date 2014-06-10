f6f05b9f8e26ce0fa3b3e59cde25ea40292e84f1
RomeoGolf <RomeoGolf@local>
2014-06-10 11:15:18 +0600

    Включена поддержка разных типов данных MatLAB
    Поддерживаются 1, 2, 4 и 8-байтные знаковые и беззнаковые целые,
    single и double

--- data_config.py
+++ data_config.py
@@ -12,14 +12,28 @@ class DataConfig:
         self.config_file.close()
 
     def GetConfigList(self):
+        _data_type = {1:'b', 2:'B', 3:'h', 4:'H', 5:'i', 6:'I', 7:'f', 9:'d', 12:'q', 13:'Q'}
+        _data_size = {1:1, 2:1, 3:2, 4:2, 5:4, 6:4, 7:4, 9:8, 12:8, 13:8}
+        # <: little-endian, 
+        '''
+        x: pad byte (no data); c:char; b:signed byte; B:unsigned byte;
+        ?: _Bool (requires C99; if not available, char is used instead)
+        h:short; H:unsigned short; i:int; I:unsigned int;
+        l:long; L:unsigned long; f:float; d:double.
+        Special case (not in native mode unless 'long long' in platform C):
+        q:long long; Q:unsigned long long
+        '''
+        
         self.config = [];    # dummy for config list
+        unused_counter = 0;
         for line in self.lines:
             var_config = line.strip().split()
             if len(var_config) == 3:
                 var_name = var_config[0]
-                var_width = int(var_config[1]) - 1
+                var_type = _data_type[int(var_config[1])]
+                var_width = _data_size[int(var_config[1])]
                 var_length = int(var_config[2])
-                var_config = var_name, var_width, var_length
+                var_config = var_name, var_width, var_length, var_type
                 self.config.append(var_config)
             if len(var_config) == 2:
                 var_name = var_config[0]
@@ -27,7 +41,9 @@ class DataConfig:
                     raise RuntimeError('Wrong name for unused bytes!')
                 var_width = 1
                 var_length = int(var_config[1])
-                var_config = var_name, var_width, var_length
+                var_name = '{}_{}'.format(var_name,  unused_counter)
+                unused_counter = unused_counter + 1
+                var_config = var_name, var_width, var_length, 'B'
                 self.config.append(var_config)
         return self.config
 
--- data_read.py
+++ data_read.py
@@ -18,21 +18,19 @@ class DataRead:
         self.block_num = os.path.getsize(data_fname) // self.dconf.packet_length
 
     def get_vars(self, df, block_num):
-        _data_type = {1:'B', 2:'H', 4:'I'}
+        # var_config = var_name, var_width, var_length, var_type
         for i in range(block_num):
             packet = df.read(self.dconf.packet_length)
             variables = {};
             index = 0
             for item in self.dconf.config:
                 s = str(item[0])
+                s1 = "<%d%s" % (item[2], item[3])
+                p = packet[index:(index+item[1]*item[2])] # get raw array
                 if item[2] == 1:    #single variable
-                    v = int.from_bytes(packet[index:(index+item[1])], 'little')
-                    variables.update({s: v})
-                else:               # array
-                    s1 = "<%d%s" % (item[2], _data_type[item[1]])
-                    # <: little-endian, H: unsigned short, I: u int, B: u byte
-                    p = packet[index:(index+item[1]*item[2])] # get raw array
+                    v = struct.unpack(s1, p)[0]
+                else:
                     v = np.asarray(struct.unpack(s1, p)) # convert to numpy arr
-                    variables.update({s: v})
+                variables.update({s: v})
                 index = index + item[1] * item[2]
             yield variables
